29a94497ffeabb0f7a0ca859009f713ad797c956
Takagi Yoshino <yoshino.51007zhu@gmail.com>
2024-08-28 12:43:35 +0900

    fix  question1

--- test_01.rb
+++ test_01.rb
@@ -1,12 +1,12 @@
 #!/usr/bin/env ruby
 A = gets.chomp.to_i
-
-if A <= 6
+a = A - 1
+if a <= 6
   puts 1
-elsif A % 6 == 0
-  puts A / 6
+elsif a % 6 == 0
+  puts a / 6
 else
-  puts (A / 6) + 1
+  puts (a / 6) + 1
 end
 
 
